55af5cab44fef5fd14195123107fb0aefab7ce4b
irooti <itoori@gmail.com>
2021-10-18 16:41:16 +0900

    union 함수 완료

--- Function1.py
+++ Function1.py
@@ -13,3 +13,47 @@ def swap(x, y):
 
 #호출
 print(swap(5, 6))
+
+print("---불변형식과 가변형식---")
+a = 1.2
+print(id(a))
+a = 2.3
+print(id(a))
+lst = [1, 2, 3]
+print(id(lst))
+lst.append(4)
+print(id(lst))
+
+#기본값이 있는 경우
+def times(a=10, b = 20):
+    return a * b
+
+#호출
+print(times())
+print(times(5))
+print(times(5, 6))
+
+#키워드인자 (파라미터명 명시)
+def connectURI(server, port):
+    strURL = "http://" + server + ":" + port
+    return strURL
+#호출
+print(connectURI("credu.com", "80"))
+print(connectURI(port="80", server="credu.com"))
+
+
+#가변인자(입력 파라미터 갯수가 변경되는 경우)
+def union(*ar):
+    #지역변수
+    result = []
+    for item in ar:
+        #단어를 슬라이싱
+        for x in item:
+            #각 글자를 슬라이싱
+            if x not in result:
+                result.append(x)
+    return result
+
+#호출
+print(union("HAM", "EGG"))
+print(union("HAM", "EGG", "SPAM"))
