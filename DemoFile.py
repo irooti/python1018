30b5267996e1538fc9124d70a0086add3f9af084
irooti <itoori@gmail.com>
2021-10-20 10:38:03 +0900

    파일연습 2

--- DemoFile.py
+++ DemoFile.py
@@ -21,3 +21,27 @@ print("{0:f}".format(4/3))
 print("{0:.2f}".format(4/3))
 print("{0:e}".format(4/3))
 print("{0:,}".format(1500000000))
+
+#파일쓰기
+f = open("c:\\work\\demo.txt", "wt", encoding='utf-8')
+f.write("첫번째라인\n두번째\n세번째\n")
+f.close()
+
+if f.closed:
+    print("파일이 정상 종료")
+else:
+    f.close()
+
+#파일일기
+f = open("c:\\work\\demo.txt", "rt", encoding='utf-8')
+print(f.read())
+print(f.tell())
+f.seek(0)
+print('---한줄씩 읽기---')
+print(f.readline())
+print(f.readline())
+print('---list로 받기---')
+f.seek(0)
+result = f.readlines()
+print(result)
+f.close()
